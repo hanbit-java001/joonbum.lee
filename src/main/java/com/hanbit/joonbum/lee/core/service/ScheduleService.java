386a38abd1b77dd6d56d3ff95b3f3d1e39878c34
hb5021 <hb5021@hb5021>
2016-09-13 10:49:05 +0900

    insert, update 수정

--- src/main/java/com/hanbit/joonbum/lee/core/service/ScheduleService.java
+++ src/main/java/com/hanbit/joonbum/lee/core/service/ScheduleService.java
@@ -7,49 +7,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
+import com.hanbit.joonbum.lee.core.dao.ScheduleDAO;
 import com.hanbit.joonbum.lee.core.vo.ScheduleVo;
 
 public class ScheduleService {
 	public int addSchedule(ScheduleVo schedule){
-		String url = "jdbc:oracle:thin:@127.0.0.1/xe";
-		String user = "hanbit";
-		String password = "hanbit";
+		ScheduleDAO scheduleDAO = new ScheduleDAO();
 
-//		String sql = "INSERT INTO SCHEDULE (SCHEDULE_ID, TITLE, MEMO, START_DT, END_DT) "
-//				+ " VALUES('" + schedule.getScheduleId() + "', "
-//				+ "'" + schedule.getTitle() + "', "
-//				+ "'" + schedule.getMemo() + "', "
-//				+ "'" + schedule.getStartDt() + "', "
-//				+ "'" + schedule.getEndDt() + "')";
-
-		String sql = "INSERT INTO SCHEDULE (SCHEDULE_ID, TITLE, MEMO, START_DT, END_DT) "
-				+ " VALUES(?, ?, ?, ?, ?)";
-
-		int result = 0;
-
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			Connection connection = DriverManager.getConnection(url, user, password);
-
-//			Statement statement = connection.createStatement();
-			PreparedStatement statement = connection.prepareStatement(sql);
-			statement.setString(1, schedule.getScheduleId());
-			statement.setString(2, schedule.getTitle());
-			statement.setString(3, schedule.getMemo());
-			statement.setString(4, schedule.getStartDt());
-			statement.setString(5, schedule.getEndDt());
-
-			result = statement.executeUpdate();
-
-			connection.close();
-
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
-
+		return scheduleDAO.insertSchedule(schedule);
 	}
 	public int modifySchedule(ScheduleVo schedule){
 
