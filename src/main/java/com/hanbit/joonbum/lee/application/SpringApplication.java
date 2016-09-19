0b188dac0d5520f0bce6b6c6694b0a4e4729c856
hb5021 <hb5021@hb5021>
2016-09-19 11:03:16 +0900

    sysdate

--- src/main/java/com/hanbit/joonbum/lee/application/SpringApplication.java
+++ src/main/java/com/hanbit/joonbum/lee/application/SpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.Log4jConfigurer;
 
+import com.hanbit.joonbum.lee.core.dao.MybatisExampleDAO;
 import com.hanbit.joonbum.lee.core.service.SchedulerService;
 import com.hanbit.joonbum.lee.core.vo.ScheduleVo;
 import com.sun.org.apache.bcel.internal.util.ClassPath;
@@ -20,17 +21,20 @@ public class SpringApplication {
 					new ClassPathXmlApplicationContext("spring/applicationContext-core.xml",
 							"spring/applicationContext-dao.xml");
 
-			SchedulerService schedulerService = applicationContext.getBean(SchedulerService.class);
-
-			ScheduleVo schedule = new ScheduleVo();
-			schedule.setScheduleId(String.valueOf(System.currentTimeMillis()));
-			schedule.setTitle("아점");
-			schedule.setMemo("돈까스");
-			schedule.setStartDt("20160917");
-			schedule.setEndDt("20160918");
-
-			int result = schedulerService.addSchedule(schedule);
-
+//			SchedulerService schedulerService = applicationContext.getBean(SchedulerService.class);
+//
+//			ScheduleVo schedule = new ScheduleVo();
+//			schedule.setScheduleId(String.valueOf(System.currentTimeMillis()));
+//			schedule.setTitle("아점");
+//			schedule.setMemo("돈까스");
+//			schedule.setStartDt("20160917");
+//			schedule.setEndDt("20160918");
+//
+//			int result = schedulerService.addSchedule(schedule);
+			
+			MybatisExampleDAO mybatisExampleDAO = applicationContext.getBean(MybatisExampleDAO.class);
+			
+			mybatisExampleDAO.logSysdate();
 
 		} catch (Exception e) {
 			e.printStackTrace();
