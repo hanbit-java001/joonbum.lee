6acb17a9507ebd6a12b4a886bfa801fecfbc8b9e
hb5021 <hb5021@hb5021>
2016-09-20 15:51:58 +0900

    달력추가

---
+++ src/main/java/com/hanbit/joonbum/lee/web/controller/ScheduleController.java
@@ -0,0 +1,17 @@
+package com.hanbit.joonbum.lee.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ScheduleController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleController.class);
+
+	@RequestMapping("/schedule/list")
+	public String list(){
+		return "schedule/list";
+	}
+}
