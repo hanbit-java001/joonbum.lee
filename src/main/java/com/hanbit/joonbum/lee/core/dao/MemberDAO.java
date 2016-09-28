edf6839af2fec76a83b0e5c6c92854a288e9b660
hb5021 <hb5021@hb5021>
2016-09-28 14:28:01 +0900

    멤버 추가

---
+++ src/main/java/com/hanbit/joonbum/lee/core/dao/MemberDAO.java
@@ -0,0 +1,39 @@
+package com.hanbit.joonbum.lee.core.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.hanbit.joonbum.lee.core.vo.MemberVO;
+
+@Repository
+public class MemberDAO {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MemberDAO.class);
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	public int countMember(String email) {
+
+		return 0;
+	}
+
+	public int insertMember(MemberVO member) {
+
+		return 0;
+	}
+
+	public String selectPassword(int memberId) {
+
+		return null;
+	}
+
+	public int updateMember(MemberVO member) {
+
+		return 0;
+	}
+
+}
